bb0e4ea6d02e5210277a3fef0620a73ea4c63488
thejareddy123 <reddytheja65@gmail.com>
2026-06-30 22:53:40 +0530

    changes in smtp

--- config.py
+++ config.py
@@ -28,6 +28,9 @@ class Config:
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_SUPPRESS_SEND = False
+    MAIL_DEBUG = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
